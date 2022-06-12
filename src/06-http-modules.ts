9c6b34b5a3110322967b9c8488913237b107f9ec
Yong Liu <yingyyliu@yahoo.com>
2022-06-12 12:38:09 +1000

    Update config files

--- src/06-http-modules.ts
+++ src/06-http-modules.ts
@@ -1,9 +1,26 @@
 import * as http from "http";
 
-const server = http.createServer((req, res) =>
-{
-  res.write(`Welcome to our home page!!!`);
-  res.end();
-});
+const portNum = 5000;
 
-server.listen(5000);
+const server = http
+  .createServer((req, res) => {
+    console.log(req);
+
+    if (req.url === "/") {
+      res.end(
+        `<html><body><h1>🔥!Welcome to our home page!🔥</h1></body></html>`
+      );
+    } else if (req.url === "/about") {
+      res.end(`<html><body><h2>Here is our short history</h2></body></html>`);
+    } else {
+      res.end(`<html><body>
+    <h1>Oops... </h1>
+    <p>We can't seem to find the age you're looking for</p>
+    <a href="/">back home</a>
+    </body></html>
+    `);
+    }
+  })
+  .listen(portNum);
+
+console.log(`Please goto: localhost:${portNum}`);
